9d67908d61b94dc21345322a902d19e4e4149e28
Swanbil <swan.bilek@bbox.fr>
2023-01-10 17:54:49 +0100

    Add Dockerfile of the app

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require("express")
 const app = express()
 
 app.get('/', (req, res) => {
-    res.status(200).json({message : 'Welcome to express'});
+    res.send('Welcome to express');
 })
 
 app.get('/test', (req, res) => {
     res.status(200).json({message : 'Test'});
 })
-app.listen(3000, () => console.log("Listening at port 3000"))
+app.listen(3000, () => console.log("Listening on port 3000"))
 
 module.exports = app;
